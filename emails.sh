ece641093d30d4eb2ba04aa000420d961d9af830
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-11-07 15:57:13 +0000

    Use passed in variable for token

--- emails.sh
+++ emails.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -i -X POST -H "Authorization: Bearer "$MR_AUTH_TOKEN"" https://meeting-rooms.cloudapps.digital/send_emails
+curl -i -X POST -H "Authorization: Bearer "$1"" https://meeting-rooms.cloudapps.digital/send_emails
